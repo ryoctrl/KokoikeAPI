4e8ecb5b9bab84d29a19427c0c8b1e71f4c7c1ae
mosin <ryoctrl@gmail.com>
2019-03-06 22:31:07 +0900

    update app.js

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const authController = require('./controllers/authController');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const shopsRouter = require('./routes/shops');
+const locationsRouter = require('./routes/locations');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,9 +22,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'kokoikeapi',
+    resave: false,
+    saveUninitialized: true
+}));
+
+authController.initialize(app);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/shops', shopsRouter);
+app.use('/locations', locationsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
