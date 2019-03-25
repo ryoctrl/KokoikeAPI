b608e869a88b59d23c99d9c88e77eecb30be5736
mosin <ryoctrl@gmail.com>
2019-03-25 17:12:39 +0900

    add tweet strategy(muni_gurume)

---
+++ strategy/muni_gurume.js
@@ -0,0 +1,20 @@
+module.exports = {
+    target: 'muni_gurume',
+    extractLocationFromTweet: function(tweet) {
+        const text = tweet.text;
+        const texts = text.split('\n');
+        const name = texts[0].slice(1, -1);
+        let areaTextLine = texts[1];
+        let areaName = '';
+        let begin = false;
+        //NOTE: 全角コロンがうまい具合にsplitやregexで取れないため以下の実装.
+        for(ch of areaTextLine) {
+            if(ch.charCodeAt(0) === 39365) break;
+            if(begin) areaName += ch;
+            if(ch.charCodeAt(0) === 65306) begin = true;
+        }
+        console.log(name, areaName);
+        return {};
+
+    }
+}
